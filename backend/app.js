d431367a146551105bdf1b52319ed8a5f3cde93d
Alejandro Forero <alejoforeroforero@gmail.com>
2023-04-13 12:37:17 -0500

    Con Token

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,16 @@ const bodyParser = require('body-parser');
 const { check } = require('express-validator');
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 app.use(bodyParser.json());
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+    next();
+})
 
 app.post('/', [
     check('nombre').not().isEmpty(),
@@ -26,10 +33,26 @@ app.post('/', [
         res.send('No se pudo hashear el password');
     }
 
+    
+
+    let token;
+
+    try{
+        token = jwt.sign(
+            { userId: nombre, email: email }, 
+            'supersecret_dont_share', 
+            { expiresIn:'1h' }
+        );
+
+    }catch(e){
+        return next(e);
+    } 
+
     const userInfo={
         nombre,
         email,
-        password:hashedPassword
+        password:hashedPassword,
+        token
     }
 
     res.json({ datos: userInfo });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [estaLogeado, setLogeado] = useState(false);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("userData"));
+
+    if (storedData && storedData.token) {
+      setLogeado(true);
+    }
+  }, []);
+
+  const login = async (event) => {
+    event.preventDefault();
+
+    try {
+      const urlLogin = `http://localhost:4000/`;
+      const response = await fetch(urlLogin, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          nombre: "Alejo",
+          email: "alejoforero@gmail.com",
+          password: "12345678",
+        }),
+      });
+      const responseData = await response.json();
+
+      if (responseData.datos.token) {
+        localStorage.setItem(
+          "userData",
+          JSON.stringify({
+            userId: responseData.datos.nombre,
+            token: responseData.datos.token,
+          })
+        );
+        setLogeado(true);
+      } else {
+        alert("Credenciales inválidas");
+      }
+    } catch (e) {}
+  };
+
+  function salir() {
+    localStorage.removeItem("userData");
+    setLogeado(false);
+  }
+
   return (
     <div className="App">
-      <div>Hola</div>
+      {estaLogeado && <button onClick={salir}>Logout</button>}
+      {!estaLogeado && (
+        <form onSubmit={login}>
+          <button type="submit">Entrar</button>
+        </form>
+      )}
     </div>
   );
 }
